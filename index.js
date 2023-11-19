54f6ec5a43c4b7d09cd73d33c1aebc0b60e5a48e
ARVAZAKI <arvazf09@gmail.com>
2023-11-19 08:28:01 +0700

    product

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.render('home')
 })
+app.get('/boku-home', (req, res) => {
+    res.render('home2')
+})
 app.get('/login', (req, res) => {
     res.render('login')
 })
-
+app.get('/register', (req, res) => {
+    res.render('register')
+})
 
 app.listen(4000, () => {
     console.log("app running well...")
